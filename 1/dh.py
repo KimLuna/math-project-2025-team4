7dd4316acb3c4db38be14fbcbe5572ecd0004f08
Seoyeong Kim <sy050623@gmail.com>
2025-11-08 07:43:41 +0000

    p3_alice.generate & sent

--- 1/dh.py
+++ 1/dh.py
@@ -20,8 +20,50 @@ def is_prime(n: int) -> bool:
     return True
 
 def find_prime_400_500() -> int:
-    """Generate a random prime number (400 < p < 500)."""
+    """Generate a random prime number (400 <= p <= 500)."""
     while True:
         p = random.randint(400, 500)
         if is_prime(p):
             return p
+
+def factorize(n: int):
+    """Return all prime factors of n."""
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            factors.append(i)
+            while n % i == 0:
+                n //= i
+        i += 1
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def is_generator(g: int, p: int) -> bool:
+    """Check whether g is a generator of Z_p*."""
+    if g <= 1 or g >= p:
+        return False
+
+    factors = factorize(p - 1)
+    for q in factors:
+        if pow(g, (p - 1) // q, p) == 1:
+            return False
+    return True
+
+def find_generator(p: int) -> int:
+    """Find a generator g for given prime p."""
+    while True:
+        g = random.randint(2, p - 2)
+        if is_generator(g, p):
+            return g
+
+import random
+
+def make_private_key(p: int) -> int:
+    """Generate a random private key for DH (2 <= a <= p-2)."""
+    return random.randint(2, p - 2)
+
+def make_public_key(g: int, a: int, p: int) -> int:
+    """Compute public key A = g^a mod p."""
+    return pow(g, a, p)
--- 1/dh_alice.py
+++ 1/dh_alice.py
@@ -4,7 +4,7 @@ import logging
 import json
 
 from aes import aes_encrypt_b64, aes_decrypt_b64
-from dh import is_prime, find_prime_400_500
+from dh import is_prime, find_prime_400_500, is_generator, make_private_key, make_public_key
 
 def run(addr, port):
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,6 +34,20 @@ def run(addr, port):
     # verify p, q
     if not is_prime(p): logging.warning("p is not prime?")
     if not is_generator(g, p): logging.warning("g is not a generator?")
+
+    # generate Alice keys(public, private)
+    a = make_private_key(p)
+    A = make_public_key(g, a, p)
+    logging.info(f"Alice private a={a}, public A={A}")
+
+    # send A to Bob
+    send_msg = {
+        "opcode": 1,
+        "type": "DH",
+        "public": A
+    }
+    conn.sendall(json.dumps(send_msg).encode("ascii"))
+    logging.info(f"[*] Sent A: {send_msg}")
 
 
     conn.close()
